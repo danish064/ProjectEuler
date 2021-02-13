63dde52a3e81f7afae1eef2885fe1a8df3a2fa27
lonleybeast <danish.danish.danish2016@gmail.com>
2021-02-13 12:05:43 +0500

    problem 8

--- src/Problem_9.java
+++ src/Problem_9.java
@@ -1,5 +1,29 @@
 public class Problem_9 {
     public static void main(String[] args) {
-        System.out.println("w");
+        /* Conditions: a < b < c
+            a + b + c = 1000
+            a^2 + b^2 = c^2
+
+            find a*b*c
+        * */
+
+        //Try one, brute force
+        for(int c = 1; c <= 1000; c++) {
+            for(int b = 1; b <= 1000; b++) {
+                for(int a = 1; a <= 1000; a++) {
+                    if(a < b && b < c) {
+                        //System.out.println("a: " + a +"\tb: "+b+"\tc: "+c);
+                        if((a*a) + (b*b) == (c*c)){
+                            //System.out.println("a: " + a +"\tb: "+b+"\tc: "+c);
+                            if(a + b + c == 1000) {
+                                System.out.println("a: " + a +"\tb: "+b+"\tc: "+c);
+                                System.out.println(a*b*c);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
